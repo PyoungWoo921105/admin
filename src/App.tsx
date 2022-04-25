da94aa4333ba5ff25557139ff4705220536afa1e
Pyoungwoo <pyoungwoo921105@gmail.com>
2022-04-25 16:37:14 +0900

    [feat] 초기 페이지 구현 시작

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+/* import logo from './logo.svg';
+import './App.css'; */
+
+import { BrowserRouter } from "react-router-dom";
+
+import Root from "../src/components/common/Root";
 
 function App() {
   return (
-    <div className="App">
+    /* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +23,10 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div> */
+    <BrowserRouter>
+      <Root></Root>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/common/Root.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { observer } from "mobx-react";
+import styled from "styled-components";
+
+import Loading from "assets/animations/Loading.svg";
+
+const RootFrame = styled.div`
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  overflow: overlay;
+`;
+
+const LoadingAnimationComponentFrame = styled.div`
+  position: fixed;
+  color: transparent;
+`;
+const LoadingAnimationComponent = styled.img`
+  color: transparent;
+`;
+
+const OutlineComponent = observer(() => {
+  return (
+    <RootFrame className="RootFrame">
+      <LoadingAnimationComponentFrame className="LoadingAnimationComponentFrame">
+        <LoadingAnimationComponent className="LoadingAnimationComponent" src={Loading}></LoadingAnimationComponent>
+      </LoadingAnimationComponentFrame>
+      ;
+    </RootFrame>
+  );
+});
+
+export default OutlineComponent;
