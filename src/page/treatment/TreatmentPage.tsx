33584d097558b21881b4039de6b56daae80b4111
Pyoungwoo <pyoungwoo921105@gmail.com>
2022-05-04 16:36:09 +0900

    [feat] 진료 부분 틀 구현 진행

--- src/page/treatment/TreatmentPage.tsx
+++ src/page/treatment/TreatmentPage.tsx
@@ -2,22 +2,10 @@ import React from 'react';
 
 import { observer } from 'mobx-react';
 
-import LogoIcon from 'assets/icons/LogoIcon.png';
-
-import { HomeFrame, LogoImageIconFrame, LogoImageIconComponent } from 'styles/components/home/Home';
+import { TreatmentFrame } from 'styles/components/treatment/Treatment';
 
 const TreatmentPage = observer(() => {
-  return (
-    <HomeFrame>
-      <LogoImageIconFrame
-        onClick={() => {
-          window.open('https://www.notion.so/MEDIR-aefcafb8bb1a45c79fdfcf58de8b9962');
-        }}
-      >
-        <LogoImageIconComponent src={LogoIcon} />
-      </LogoImageIconFrame>
-    </HomeFrame>
-  );
+  return <TreatmentFrame />;
 });
 
 export default TreatmentPage;
